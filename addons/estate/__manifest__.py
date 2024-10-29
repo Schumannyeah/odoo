bd61d8bc358cd39cf666e15e548212c3b1167e2b
Schumannyeah <schumannyeah@hotmail.com>
2024-10-29 16:53:27 +0800

    office_2024102904

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -10,4 +10,9 @@
     'installable': True,
     'application': True,
     'auto_install': False,
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+    ],
 }
